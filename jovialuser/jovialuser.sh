ac8ca1a0057bf8ea1c9b195d24bd3ede6e22d504
root <root@cassaca-node008.cm.cluster>
2017-09-01 15:25:15 -0300

    ignore

--- jovialuser/jovialuser.sh
+++ jovialuser/jovialuser.sh
@@ -16,8 +16,6 @@ export PATH="${HOME}/.conda/bin:$PATH"
 if [ ! -d "/home/$USER/.conda" ]
 then
 	su - $USER -c "/bin/bash /conda-installer.sh -f -b -p /home/$USER/.conda"
-	su - $USER -c "/home/$USER/.conda/bin/conda config --system --set auto_update_conda false"
-	su - $USER -c "/home/$USER/.conda/bin/conda  install  -y notebook jupyterlab ipython ipykernel python numpy qt astropy nomkl ipywidgets=6.0* pandas=0.19*  numexpr=2.6.* matplotlib=2.0* scipy=0.19* seaborn=0.7* scikit-learn=0.18* scikit-image=0.12* sympy=1.0* cython=0.25* patsy=0.4* statsmodels=0.8* cloudpickle=0.2* dill=0.2* numba=0.31* bokeh=0.12* sqlalchemy=1.1* hdf5=1.8.17 h5py=2.6* vincent=0.4.* beautifulsoup4=4.5.* xlrd"
 	su - $USER -c "/home/$USER/.conda/bin/pip install jupyterhub==0.8.0.b3"
 fi
 
