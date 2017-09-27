c8db88326480e602f51990d277120aab843492c8
Matias Diaz <madiaz@monty.csrg.inf.utfsm.cl>
2017-09-27 13:43:18 -0300

    change registry in ./build script

--- jovialuser/build.sh
+++ jovialuser/build.sh
@@ -1,3 +1,3 @@
 set -xe
-docker build --rm -t 10.6.91.18:5000/jovial/jovialuser .
-docker push 10.6.91.18:5000/jovial/jovialuser
+docker build --rm -t 10.6.91.15:5000/jovial/jovialuser .
+docker push 10.6.91.15:5000/jovial/jovialuser
