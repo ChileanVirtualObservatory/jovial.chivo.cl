5c790ea2952401f499f72211225e8358d092dbe9
Carlos Ponce <cponce@alumnos.inf.utfsm.cl>
2018-04-30 20:33:13 -0300

    Added conda installation steps to installer container

--- docker/conda-installer/installer.sh
+++ docker/conda-installer/installer.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 set -xe
-env
+USER_ID=$(stat -c "%u" $HOME)
+CONDA_DIR=$HOME/.conda/
+SUCCESS_FLAG=$CONDA_DIR/.jovial_install_successful
+useradd -u $USER_ID /bin/bash $USER
+if [ ! -f $SUCCESS_FLAG ]; then
+	echo "Installing conda"
+  su - $USER -c "/bin/bash /conda-installer.sh -f -b -p $CONDA_DIR"
+fi
+touch $SUCCESS_FLAG
 echo 'Done'
