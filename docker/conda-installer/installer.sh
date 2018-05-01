3dcc4066a08f721b5019400bd0d965fe5a9004a2
Carlos Ponce <cponce@alumnos.inf.utfsm.cl>
2018-04-30 22:23:45 -0300

    Added code to check if conda directory already exists

--- docker/conda-installer/installer.sh
+++ docker/conda-installer/installer.sh
@@ -3,10 +3,14 @@ set -xe
 USER_ID=$(stat -c "%u" $HOME)
 CONDA_DIR=$HOME/.conda/
 SUCCESS_FLAG=$CONDA_DIR/.jovial_install_successful
-adduser -u $USER_ID -s /bin/bash $USER
+adduser -u $USER_ID -s /bin/bash -D -g "" $USER
 if [ ! -f $SUCCESS_FLAG ]; then
 	echo "Installing conda"
+  if [ -d $CONDA_DIR ]; then
+    echo "Conda detected, removing corrupted directory"
+		rm -rf $CONDA_DIR
+  fi
   su - $USER -c "/bin/bash /conda-installer.sh -f -b -p $CONDA_DIR"
+  touch $SUCCESS_FLAG
 fi
-touch $SUCCESS_FLAG
-echo 'Done'
+echo 'Installation Completed'
