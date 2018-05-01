4017b032148043c247a13dab4f16757d3b482ccb
Carlos Ponce <cponce@alumnos.inf.utfsm.cl>
2018-04-30 23:00:05 -0300

    Added missing old installation steps

--- docker/conda-installer/installer.sh
+++ docker/conda-installer/installer.sh
@@ -10,7 +10,9 @@ if [ ! -f $SUCCESS_FLAG ]; then
     echo "Conda detected, removing corrupted directory"
 		rm -rf $CONDA_DIR
   fi
+	su - $USER -c "grep -q PATH=$CONDA_DIR/bin:$PATH $HOME/.bashrc || echo PATH=$CONDA_DIR/bin:$PATH >> $HOME/.bashrc"
   su - $USER -c "/bin/bash /conda-installer.sh -f -b -p $CONDA_DIR"
+	su - $USER -c "yes | $CONDA_DIR/bin/pip install jupyterhub acalib"
   touch $SUCCESS_FLAG
 fi
 echo 'Installation Completed'
