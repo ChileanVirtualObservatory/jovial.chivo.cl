b59ef39a36d5c97e2557679e8840e02551e2c382
Carlos Ponce <cponce@alumnos.inf.utfsm.cl>
2018-05-01 19:44:22 -0300

    Added JOVIAL_NOTEBOOK_EXTRA_ARGS environment variable to user pod

--- docker/user/start.sh
+++ docker/user/start.sh
@@ -3,4 +3,4 @@ set -xe
 CONDA_DIR=$HOME/.conda
 USER_ID=$(stat -c "%u" $HOME)
 adduser -u $USER_ID --shell /bin/bash --disabled-password --gecos "" $USER
-su -p $USER -c "$CONDA_DIR/bin/jupyterhub-singleuser --ip=0.0.0.0"
+su -p $USER -c "$CONDA_DIR/bin/jupyterhub-singleuser --ip=0.0.0.0 $JOVIAL_NOTEBOOK_EXTRA_ARGS"
