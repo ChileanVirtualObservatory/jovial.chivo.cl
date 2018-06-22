f3fe7e7a4e4c3313b7493a060218b6af166f29ae
Carlos Ponce <cponce@alumnos.inf.utfsm.cl>
2018-06-21 21:09:04 -0400

    Fixed user UID mapping

--- docker/user/start.sh
+++ docker/user/start.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -xe
 CONDA_DIR=$HOME/.conda
-USER_ID=$(stat -c "%u" $HOME)
-adduser -u $USER_ID --shell /bin/bash --disabled-password --gecos "" $USER
+adduser -u $JOVIAL_NOTEBOOK_USER_UID --shell /bin/bash --disabled-password --gecos "" $USER
 su -p $USER -c "$CONDA_DIR/bin/jupyterhub-singleuser --ip=0.0.0.0 $JOVIAL_NOTEBOOK_EXTRA_ARGS"
